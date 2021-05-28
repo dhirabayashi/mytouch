39cdaaaab392ffc4d2d6db2ea00e3aa7c1441fbd
daiki <gigabowser@hotmail.co.jp>
2021-05-28 21:58:22 +0900

    既存ファイルの場合、最終更新日時を書き換える処理を実装

--- src/main/kotlin/com/github/dhirabayashi/mytouch/Main.kt
+++ src/main/kotlin/com/github/dhirabayashi/mytouch/Main.kt
@@ -3,6 +3,12 @@ package com.github.dhirabayashi.mytouch
 import com.beust.jcommander.JCommander
 import java.nio.file.Files
 import java.nio.file.Path
+import java.nio.file.attribute.FileTime
+import java.time.Clock
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+
+var clock: Clock = Clock.systemDefaultZone()
 
 fun main(args: Array<String>) {
     val argument = Argument()
@@ -16,6 +22,12 @@ fun main(args: Array<String>) {
     }
 }
 
-fun touch(file: String) {
-    Files.createFile(Path.of(file))
+fun touch(filename: String) {
+    val file = Path.of(filename)
+    if(Files.exists(file)) {
+        val now = LocalDateTime.now(clock).toInstant(ZoneOffset.ofHours(9)).toEpochMilli()
+        Files.setLastModifiedTime(file, FileTime.fromMillis(now))
+    } else {
+        Files.createFile(file)
+    }
 }
--- src/test/kotlin/com/github/dhirabayashi/mytouch/MainKtTest.kt
+++ src/test/kotlin/com/github/dhirabayashi/mytouch/MainKtTest.kt
@@ -6,11 +6,16 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.io.TempDir
 import java.nio.file.Files
 import java.nio.file.Path
+import java.nio.file.attribute.FileTime
+import java.time.Clock
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZoneOffset
 
 internal class MainKtTest {
 
     @Test
-    fun test_touch(@TempDir tempDir: Path) {
+    fun test_touch_newFile(@TempDir tempDir: Path) {
         // setup
         val file = tempDir.resolve("test.txt")
 
@@ -19,5 +24,23 @@ internal class MainKtTest {
 
         // verify
         assertTrue(Files.exists(file))
+    }
+
+    @Test
+    fun test_touch_existingFile(@TempDir tempDir: Path) {
+        // setup
+        val file = tempDir.resolve("test.txt")
+        Files.createFile(file)
+
+        val ldt = LocalDateTime.of(2021, 5, 28, 21, 53)
+        val instant = ldt.toInstant(ZoneOffset.ofHours(9))
+        clock = Clock.fixed(instant, ZoneId.of("Asia/Tokyo"))
+
+        // run
+        touch(file.toAbsolutePath().toString())
+
+        // verify
+        assertTrue(Files.exists(file))
+        assertEquals(FileTime.fromMillis(instant.toEpochMilli()), Files.getLastModifiedTime(file))
     }
 }
